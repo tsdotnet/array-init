cfdae8ad61ed3bf8c5957c3629d473d7431375d3
electricessence <5899455+electricessence@users.noreply.github.com>
2025-08-16 01:42:18 -0700

    fix: Generate proper ESM modules instead of CommonJS
    - Update tsconfig.esm.json to use module: esnext and moduleResolution: bundler
    - ESM build now generates true ES modules with import/export syntax
    - Fixes violation where ESM build was generating CommonJS code with require/exports

--- dist/esm/arrayInit.js
+++ dist/esm/arrayInit.js
@@ -1,17 +1,14 @@
-"use strict";
 /*!
  * @author electricessence / https://github.com/electricessence/
  * Licensing: MIT
  */
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = arrayInit;
 /**
  * Initializes an array depending on the requested capacity.
  * The returned array will have a .length equal to the value provided.
  * @param length
  * @returns {[]}
  */
-function arrayInit(length) {
+export default function arrayInit(length) {
     // This logic is based upon JS performance tests that show a significant difference at the level of 65536.
     let array;
     if (length > 65536)
