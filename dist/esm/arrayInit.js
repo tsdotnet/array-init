05da6afdcf152b443c9a0e1dfcf66648938b8183
electricessence <5899455+electricessence@users.noreply.github.com>
2025-08-15 23:38:46 -0700

    feat: modernize to dual module system with separate folder structure
    - Implement separate dist folders (cjs/, esm/, umd/) for clean module separation
    - Add UMD build support for direct web usage
    - Update tsconfig inheritance pattern with format-specific configs
    - Switch from ts-node to tsx for improved testing performance
    - Update package.json exports for seamless dual module importing
    - Remove old mixed build artifacts

--- dist/esm/arrayInit.js
+++ dist/esm/arrayInit.js
@@ -1,14 +1,17 @@
+"use strict";
 /*!
  * @author electricessence / https://github.com/electricessence/
  * Licensing: MIT
  */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = arrayInit;
 /**
  * Initializes an array depending on the requested capacity.
  * The returned array will have a .length equal to the value provided.
  * @param length
  * @returns {[]}
  */
-export default function arrayInit(length) {
+function arrayInit(length) {
     // This logic is based upon JS performance tests that show a significant difference at the level of 65536.
     let array;
     if (length > 65536)
---
+++ dist/umd/arrayInit.js
@@ -0,0 +1,35 @@
+/*!
+ * @author electricessence / https://github.com/electricessence/
+ * Licensing: MIT
+ */
+(function (factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        var v = factory(require, exports);
+        if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === "function" && define.amd) {
+        define(["require", "exports"], factory);
+    }
+})(function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.default = arrayInit;
+    /**
+     * Initializes an array depending on the requested capacity.
+     * The returned array will have a .length equal to the value provided.
+     * @param length
+     * @returns {[]}
+     */
+    function arrayInit(length) {
+        // This logic is based upon JS performance tests that show a significant difference at the level of 65536.
+        let array;
+        if (length > 65536)
+            array = new Array(length);
+        else {
+            array = [];
+            array.length = length;
+        }
+        return array;
+    }
+});
+//# sourceMappingURL=arrayInit.js.map
